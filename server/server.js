b3fae6cbe127951184cfe5016ecba8fd105eac45
kasmr <alikkras@gmail.com>
2022-01-03 19:23:01 +0300

    Created server

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+
+const messages = [];
+
+const typeDefs = `
+type Message {
+    id: ID!
+    user: String!
+    content: String!
+}
+
+type Query {
+    getMessages: [Message!]
+}
+
+type Mutation {
+    postMessage(user: String!, content: String!): ID!
+}
+`;
+
+const resolvers = {
+    Query: {
+        getMessages: () => messages,
+    },
+    Mutation: {
+        postMessage: (parent, { user, content }) => {
+            const id = messages.length;
+            messages.push({
+                id,
+                user,
+                content,
+            });
+            return id;
+        },
+    },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(({ port }) => {
+    console.log(`Server on http://localhost:${port}/`);
+});
+
+
